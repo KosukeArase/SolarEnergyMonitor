b5d016c10c6c76e5fb15893c4cd6a119369c1607
KosukeArase <kosuke.arase@gmail.com>
2024-04-01 15:30:21 +0900

    initial commit

---
+++ SolarEnergyMonitor/SolarEnergyMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarEnergyMonitorApp.swift
+//  SolarEnergyMonitor
+//
+//  Created by Kosuke Arase on 2024/04/01.
+//
+
+import SwiftUI
+
+@main
+struct SolarEnergyMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
